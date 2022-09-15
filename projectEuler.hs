2ef9d37cba91de4021a388800cdba610ec512bf1
Sara Betancourth <sarabet93@gmail.com>
2022-09-15 15:09:09 -0400

    comment updates

--- projectEuler.hs
+++ projectEuler.hs
@@ -226,6 +226,18 @@ fib n = go n 1 1
 
 
 
+-- 77. Prime summations
+-- It is possible to write ten as the sum of primes in exactly five different ways:
+-- 7 + 3
+-- 5 + 5
+-- 5 + 3 + 2
+-- 3 + 3 + 2 + 2
+-- 2 + 2 + 2 + 2 + 2
+-- What is the first value which can be written as the sum of primes in over five thousand different ways?
+
+
+
+
 -- 92. Square digit chains
 -- A number chain is created by continuously adding the square of the digits in a number to form a new number until it has been seen before.
 -- For example,
